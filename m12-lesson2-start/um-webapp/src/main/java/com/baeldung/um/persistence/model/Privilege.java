37d0eb1981e411b99cdce94f61232380c3f7682d
loredana crusoveanu <lore.crusoveanu@gmail.com>
2018-07-18 20:07:24 +0300

    rename modules, remove lombok

--- m12-lesson2-start/um-webapp/src/main/java/com/baeldung/um/persistence/model/Privilege.java
+++ m12-lesson2-start/um-webapp/src/main/java/com/baeldung/um/persistence/model/Privilege.java
@@ -1,29 +1,17 @@
 package com.baeldung.um.persistence.model;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.baeldung.common.interfaces.INameableDto;
 import com.baeldung.common.persistence.model.INameableEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Entity
-@Data
-@Getter @Setter @EqualsAndHashCode(of = "name") @ToString(of = "name")
+@XmlRootElement
 public class Privilege implements INameableEntity, INameableDto {
 
     @Id
@@ -34,12 +22,8 @@ public class Privilege implements INameableEntity, INameableDto {
     @Column(unique = true, nullable = false)
     private String name;
 
-    @Column()
+    @Column(unique = false, nullable = true)
     private String description;
-
-    @JsonIgnore
-    @ManyToMany(mappedBy = "privileges", fetch = FetchType.EAGER)
-    private Set<Role> roles;
 
     public Privilege() {
         super();
@@ -49,4 +33,66 @@ public class Privilege implements INameableEntity, INameableDto {
         super();
         name = nameToSet;
     }
+
+    // API
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(final Long idToSet) {
+        id = idToSet;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String nameToSet) {
+        name = nameToSet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String descriptionToSet) {
+        description = descriptionToSet;
+    }
+
+    //
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Privilege other = (Privilege) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Privilege [name=" + name + ", description=" + description + "]";
+    }
+
 }
--- m12-lesson2-start/um-webapp/src/main/java/com/baeldung/um/persistence/model/Role.java
+++ m12-lesson2-start/um-webapp/src/main/java/com/baeldung/um/persistence/model/Role.java
@@ -11,25 +11,15 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.baeldung.common.interfaces.INameableDto;
 import com.baeldung.common.persistence.model.INameableEntity;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import com.baeldung.common.interfaces.INameableDto;
-import com.baeldung.common.persistence.model.INameableEntity;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 @Entity
-@Data
-@Getter @Setter @EqualsAndHashCode(of = "name") @ToString(of = { "id", "name"})
+@XmlRootElement
 public class Role implements INameableEntity, INameableDto {
 
     @Id
@@ -41,8 +31,8 @@ public class Role implements INameableEntity, INameableDto {
     private String name;
 
     // @formatter:off
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(joinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ROLE_ID")}, inverseJoinColumns = {@JoinColumn(name = "PRIV_ID", referencedColumnName = "PRIV_ID")})
+    @ManyToMany( /* cascade = { CascadeType.REMOVE }, */fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = { @JoinColumn(name = "ROLE_ID", referencedColumnName = "ROLE_ID") }, inverseJoinColumns = { @JoinColumn(name = "PRIV_ID", referencedColumnName = "PRIV_ID") })
     private Set<Privilege> privileges;
     // @formatter:on
 
@@ -60,4 +50,66 @@ public class Role implements INameableEntity, INameableDto {
         name = nameToSet;
         privileges = privilegesToSet;
     }
+
+    // API
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(final Long idToSet) {
+        id = idToSet;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String nameToSet) {
+        name = nameToSet;
+    }
+
+    public Set<Privilege> getPrivileges() {
+        return privileges;
+    }
+
+    public void setPrivileges(final Set<Privilege> privilegesToSet) {
+        privileges = privilegesToSet;
+    }
+
+    //
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Role other = (Role) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("id", id).append("name", name).toString();
+    }
+
 }
