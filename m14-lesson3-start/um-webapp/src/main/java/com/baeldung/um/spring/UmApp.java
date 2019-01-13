c8ec3a8e831d58ebea28eb170fe70f88f07dbd69
Loredana <lore.crusoveanu@gmail.com>
2019-01-13 21:38:30 +0200

    updates

--- m14-lesson3-start/um-webapp/src/main/java/com/baeldung/um/spring/UmApp.java
+++ m14-lesson3-start/um-webapp/src/main/java/com/baeldung/um/spring/UmApp.java
@@ -2,12 +2,11 @@ package com.baeldung.um.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.baeldung.um.persistence.setup.MyApplicationContextInitializer;
 
 @SpringBootApplication
-public class UmApp extends SpringBootServletInitializer {
+public class UmApp {
 
     public static void main(final String... args) {
         new SpringApplicationBuilder(UmApp.class).initializers(new MyApplicationContextInitializer()).run(args);
