6f6750b4b3985d3a80dd871fc2dd29d37605137c
Loredana <lore.crusoveanu@gmail.com>
2019-02-12 09:23:07 +0200

    upgrades, fix angularjs, remove httpcore

--- m6-lesson2/um-webapp/src/main/java/com/baeldung/um/run/UmApp.java
+++ m6-lesson2/um-webapp/src/main/java/com/baeldung/um/run/UmApp.java
@@ -3,8 +3,6 @@ package com.baeldung.um.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,20 +16,18 @@ import com.baeldung.um.spring.ResourceServerConfiguration;
 import com.baeldung.um.spring.UmContextConfig;
 import com.baeldung.um.spring.UmPersistenceJpaConfig;
 import com.baeldung.um.spring.UmServiceConfig;
-import com.baeldung.um.spring.UmServletConfig;
 import com.baeldung.um.spring.UmWebConfig;
 
 @SpringBootApplication(exclude = { // @formatter:off
         ErrorMvcAutoConfiguration.class
 })// @formatter:on
-public class UmApp extends SpringBootServletInitializer {
+public class UmApp {
 
     private final static Class[] CONFIGS = { // @formatter:off
             UmContextConfig.class,
             UmPersistenceJpaConfig.class,
             UmServiceConfig.class,
-            UmWebConfig.class,
-            UmServletConfig.class,
+            UmWebConfig.class,            
 
             UmApp.class,
 
@@ -39,12 +35,7 @@ public class UmApp extends SpringBootServletInitializer {
             AuthorizationServerConfiguration.class
     }; // // @formatter:on
 
-    //
-
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-        return application.sources(CONFIGS).initializers(new MyApplicationContextInitializer());
-    }
+    //    
 
     public static void main(final String... args) {
         final SpringApplication springApplication = new SpringApplication(CONFIGS);
--- m6-lesson2/um-webapp/src/main/java/com/baeldung/um/spring/AuthorizationServerConfiguration.java
+++ m6-lesson2/um-webapp/src/main/java/com/baeldung/um/spring/AuthorizationServerConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
