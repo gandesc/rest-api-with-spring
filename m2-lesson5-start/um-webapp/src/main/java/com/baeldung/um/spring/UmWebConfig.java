2306e0f950e4027aa0d749d79658bcfb166f8789
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-25 15:29:58 +0200

    enabled & customized xml support

--- m2-lesson5-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
+++ m2-lesson5-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
@@ -1,21 +1,21 @@
 package com.baeldung.um.spring;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
-@ComponentScan({ "com.baeldung.um.web", "com.baeldung.common.web" })
+@ComponentScan({"com.baeldung.um.web", "com.baeldung.common.web"})
 @EnableWebMvc
 public class UmWebConfig implements WebMvcConfigurer {
 
@@ -24,14 +24,24 @@ public class UmWebConfig implements WebMvcConfigurer {
     @Override
     public void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
         final Optional<HttpMessageConverter<?>> jsonConverterFound = converters.stream()
-            .filter(c -> c instanceof MappingJackson2HttpMessageConverter)
-            .findFirst();
+                .filter(c -> c instanceof MappingJackson2HttpMessageConverter)
+                .findFirst();
         if (jsonConverterFound.isPresent()) {
             final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) jsonConverterFound.get();
             converter.getObjectMapper()
-                .enable(SerializationFeature.INDENT_OUTPUT);
+                    .enable(SerializationFeature.INDENT_OUTPUT);
             converter.getObjectMapper()
-                .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                    .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        }
+
+        final Optional<HttpMessageConverter<?>> xmlConverterFound = converters.stream()
+                .filter(c -> c instanceof MappingJackson2XmlHttpMessageConverter)
+                .findFirst();
+        if (xmlConverterFound.isPresent()) {
+            final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) xmlConverterFound.get();
+            converter.getObjectMapper()
+                    .enable(SerializationFeature.INDENT_OUTPUT)
+                    .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         }
     }
 }
