d42a30b016c60373243a4cef189ee9f10024848b
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-29 14:24:12 +0200

    added new client methods

--- m5-lesson2-start/um-client/src/test/java/com/baeldung/um/client/template/RoleSimpleApiClient.java
+++ m5-lesson2-start/um-client/src/test/java/com/baeldung/um/client/template/RoleSimpleApiClient.java
@@ -1,5 +1,14 @@
 package com.baeldung.um.client.template;
 
+import com.baeldung.common.spring.util.Profiles;
+import com.baeldung.um.client.UmPaths;
+import com.baeldung.um.persistence.model.Role;
+import com.baeldung.um.util.Um;
+import com.google.common.base.Preconditions;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +16,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
-import com.baeldung.common.spring.util.Profiles;
-import com.baeldung.um.client.UmPaths;
-import com.baeldung.um.util.Um;
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
+import java.util.List;
 
 @Component
 @Profile(Profiles.CLIENT)
@@ -24,9 +29,26 @@ public final class RoleSimpleApiClient {
 
     // API
 
-    // find - one
+    public final Role findOne(final long id) {
+        final Response response = findOneAsResponse(id);
+        Preconditions.checkState(response.getStatusCode() == 200, "Find one operation didn't result in a 200 ok")
+
+        return response.as(Role.class);
+    }
+
+    public final Response findOneAsResponse(final long id) {
+        return read(getUri() + "/" + id);
+    }
+
+    public final List<Role> findAll() {
+        return read(getUri()).as(List.class);
+    }
 
     // UTIL
+
+    private Response read(final String uri) {
+        return givenAuthenticated().accept(JSON).get(uri);
+    }
 
     public final String getUri() {
         return paths.getRoleUri();
@@ -35,9 +57,9 @@ public final class RoleSimpleApiClient {
     public final RequestSpecification givenAuthenticated() {
         final Pair<String, String> credentials = getDefaultCredentials();
         return RestAssured.given()
-            .auth()
-            .preemptive()
-            .basic(credentials.getLeft(), credentials.getRight());
+                .auth()
+                .preemptive()
+                .basic(credentials.getLeft(), credentials.getRight());
     }
 
     private final Pair<String, String> getDefaultCredentials() {
