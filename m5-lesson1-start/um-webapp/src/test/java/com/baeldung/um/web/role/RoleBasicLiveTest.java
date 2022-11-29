ad802e770166482f2d1edb3e773d72c15f9beba7
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-29 13:27:58 +0200

    added first live test

--- m5-lesson1-start/um-webapp/src/test/java/com/baeldung/um/web/role/RoleBasicLiveTest.java
+++ m5-lesson1-start/um-webapp/src/test/java/com/baeldung/um/web/role/RoleBasicLiveTest.java
@@ -3,6 +3,14 @@ package com.baeldung.um.web.role;
 import static com.baeldung.common.spring.util.Profiles.CLIENT;
 import static com.baeldung.common.spring.util.Profiles.TEST;
 
+import com.baeldung.um.util.Um;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,5 +28,16 @@ import com.baeldung.um.spring.UmLiveTestConfig;
 public class RoleBasicLiveTest {
 
     private final static String JSON = MediaType.APPLICATION_JSON.toString();
+
+    @Test
+    public void whenAllRolesAreRetrieved_then200OK() {
+        final String uri = "http://localhost:8082/um-webapp/api/roles";
+
+        final RequestSpecification basicAuth = RestAssured.given()
+                .auth().preemptive().basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS);
+        final Response response = basicAuth.accept(ContentType.JSON).get(uri);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
 
 }
