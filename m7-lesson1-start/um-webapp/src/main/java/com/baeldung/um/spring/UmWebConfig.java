610d00bb174e560d157be18731d3e2e3126ca25b
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-12-07 16:11:44 +0200

    upgraded to the new open api dependency

--- m7-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
+++ m7-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
@@ -1,46 +1,16 @@
 package com.baeldung.um.spring;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.LocalDate;
 
 @Configuration
 @ComponentScan({"com.baeldung.common.web", "com.baeldung.um.web"})
 @EnableWebMvc
-@EnableSwagger2
 public class UmWebConfig implements WebMvcConfigurer {
 
     public UmWebConfig() {
         super();
-    }
-
-    @Bean
-    public Docket mainConfig() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/api")
-                .directModelSubstitute(LocalDate.class, String.class)
-                .genericModelSubstitutes(ResponseEntity.class);
-    }
-
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
