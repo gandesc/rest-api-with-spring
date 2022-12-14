bf96a4db341c33f6c100a984bac311118416682d
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-12-14 16:24:00 +0200

    wip; used webclient in a simple test

--- m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/LiveTestConfig.java
+++ m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/LiveTestConfig.java
@@ -1,10 +1,22 @@
 package com.baeldung.um.live;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class LiveTestConfig {
 
     private final static String BASE_URI = "http://localhost:8082/api";
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(BASE_URI)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 
 }
--- m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/WebClientLiveTest.java
+++ m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/WebClientLiveTest.java
@@ -1,11 +1,33 @@
 package com.baeldung.um.live;
 
+import com.baeldung.um.persistence.model.Privilege;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { LiveTestConfig.class })
 public class WebClientLiveTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void whenRetrievingPrivileges_thenOk() throws InterruptedException {
+        Flux<Privilege> result = webClient.get()
+                .uri("/privileges")
+                .accept(MediaType.valueOf(MediaType.TEXT_EVENT_STREAM_VALUE))
+                .retrieve()
+                .bodyToFlux(Privilege.class);
+
+        result.subscribe(privilege -> System.out.println(privilege));
+
+        Thread.sleep(35000);
+    }
 
 }
