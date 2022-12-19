3886e7280ea9c50c2e8248477af97715a207d3c3
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-12-19 14:08:54 +0200

    autoconfig exclusion & logging demo

--- m14-lesson3-start/um-webapp/src/main/java/com/baeldung/um/spring/UmApp.java
+++ m14-lesson3-start/um-webapp/src/main/java/com/baeldung/um/spring/UmApp.java
@@ -1,11 +1,12 @@
 package com.baeldung.um.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.baeldung.um.persistence.setup.MyApplicationContextInitializer;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = JacksonAutoConfiguration.class)
 public class UmApp {
 
     public static void main(final String... args) {
